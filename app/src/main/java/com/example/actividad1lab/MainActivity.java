043766bab6489cb41deb781ea7e5265ca55a03c1
FaustoDavid98 <fdavid@frsf.utn.edu.ar>
2020-08-28 22:08:10 -0300

    Interfaz y listener del switch

--- app/src/main/java/com/example/actividad1lab/MainActivity.java
+++ app/src/main/java/com/example/actividad1lab/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.actividad1lab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+    Switch switchCarga; //Defino el switch
+    SeekBar cantidadCarga; //Defino el seekbar
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        switchCarga = findViewById(R.id.cargaInicial);
+        cantidadCarga = findViewById(R.id.barraCarga);
+
+        switchCarga.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    cantidadCarga.setVisibility(View.VISIBLE);
+                } else {
+                    cantidadCarga.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
     }
 }
